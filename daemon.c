9761ede1597b079af927071d9c0631be0935485a
Mingqiang Sun <davidmusksun1993@gmail.com>
2022-07-19 13:14:20 +0800

    fix 词不达意
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

--- daemon.c
+++ daemon.c
@@ -83,69 +83,70 @@ _main() {
     signal(SIGPIPE, SIG_IGN);
     int i, n, rc = -1;
     const int subscriberSendBufSize = 16 * 1024 * 1024;
-    autoFd(serverfd);
-    autoFd(epollfd);
+    autoFd(serverFd);
+    autoFd(epollFd);
     struct epoll_event events[DAEMON_MAX_EVENTS], ev;
-    peerManager pm;
-    _attr(aligned(8)) char buf[LOGBUFLEN];
-    char msgbuf[LOGMSGLEN];
-    msgReqInit *reqinit = (msgReqInit *) buf;
-    msgLog *log = (msgLog *) buf;
-    pubEntry se;
+    peerManager manager;
+    _attr(aligned(8)) char protocolBuffer[LOGBUFLEN];
+    char messageBuffer[LOGMSGLEN];
+    msgReqInit *initRequest = (msgReqInit *) protocolBuffer;
+    msgLog *log = (msgLog *) protocolBuffer;
+    pubEntry serverEntry;
     const char *logFmt;
 
-    unixStreamServer(rc, serverfd, LOGIPC);
+    unixStreamServer(rc, serverFd, LOGIPC);
     if (rc == -1) return DAEMON_ERROR_SERVER;
-    se.fd = serverfd;
-    epollfd = epoll_create1(0);
+    serverEntry.fd = serverFd;
+    epollFd = epoll_create1(0);
     memset(&ev, 0, sizeof(ev));
-    ev.data.ptr = &se;
+    ev.data.ptr = &serverEntry;
     ev.events = EPOLLIN;
-    epoll_ctl(epollfd, EPOLL_CTL_ADD, serverfd, &ev);
-    initPeerManager(&pm);
+    epoll_ctl(epollFd, EPOLL_CTL_ADD, serverFd, &ev);
+    initPeerManager(&manager);
 
     for (;;) {
-        _poll(rc, epoll_wait, epollfd, events, DAEMON_MAX_EVENTS, -1);
+        _poll(rc, epoll_wait, epollFd, events, DAEMON_MAX_EVENTS, -1);
         for (i = 0; i < rc; i++) {
             int fd = ((pubEntry *) events[i].data.ptr)->fd;
-            if (fd == serverfd) {
+            if (fd == serverFd) {
                 autoFd(peerFd);
                 peerFd = accept4(fd, 0, 0, O_NONBLOCK);
                 if (peerFd == -1) return DAEMON_ERROR_ACCEPT;
-                if (!limitRead(peerFd, sizeof(msgReqInit), buf, LOGBUFLEN)) {
+                if (!limitRead(peerFd, sizeof(msgReqInit), protocolBuffer, LOGBUFLEN)) {
                     log2("Error occurs on peer init");
                     continue;
                 }
-                if (reqinit->role == LOG_ROLE_PUB) {
-                    ev.data.ptr = newPub(&pm, peerFd, reqinit);
-                    epoll_ctl(epollfd, EPOLL_CTL_ADD, peerFd, &ev);
+                if (initRequest->role == LOG_ROLE_PUB) {
+                    ev.data.ptr = newPub(&manager, peerFd, initRequest);
+                    epoll_ctl(epollFd, EPOLL_CTL_ADD, peerFd, &ev);
                     log1("New publisher #%d", peerFd);
-                } else if (reqinit->role == LOG_ROLE_SUB) {
-                    newSub(&pm, peerFd);
+                } else if (initRequest->role == LOG_ROLE_SUB) {
+                    newSub(&manager, peerFd);
                     log1("New subscriber #%d", peerFd);
                     setsockopt(peerFd, SOL_SOCKET, SO_SNDBUF, &subscriberSendBufSize, sizeof(subscriberSendBufSize));
                 }
-                msgResInit resinit = {0};
-                write(peerFd, &resinit, sizeof resinit);
+                msgResInit initResponse = {0};
+                write(peerFd, &initResponse, sizeof initResponse);
                 peerFd = -1;
             } else {
                 pubEntry *entry = events[i].data.ptr;
-                if (events[i].events & (EPOLLERR | EPOLLHUP) || !limitRead(fd, sizeof(msgLog), buf, LOGBUFLEN)) {
-                    freePub(&pm, entry);
+                if (events[i].events & (EPOLLERR | EPOLLHUP) ||
+                    !limitRead(fd, sizeof(msgLog), protocolBuffer, LOGBUFLEN)) {
+                    freePub(&manager, entry);
                     log1("Publisher #%d leave", fd);
                 } else {
                     logFmt = log->data[log->len - 1] == '\n' ? LOG_FMT : LOG_FMT "\n";
-                    n = sprintf(msgbuf, logFmt,
+                    n = sprintf(messageBuffer, logFmt,
                                 log->sec, log->us,
                                 entry->pid, log->tid,
                                 entry->len, entry->tag,
                                 logLevel2String(log->level),
                                 log->len, log->data);
-                    postMessage(&pm, msgbuf, n);
+                    postMessage(&manager, messageBuffer, n);
                 }
             }
         }
     }
-    freePeerManager(&pm);
+    freePeerManager(&manager);
     return 0;
 }
