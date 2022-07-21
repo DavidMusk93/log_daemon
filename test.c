48e39ac062b606df94de086c19932d02b13a5b7b
Mingqiang Sun <davidmusksun1993@gmail.com>
2022-07-21 21:36:53 +0800

    add options to logtest
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

--- test.c
+++ test.c
@@ -1,20 +1,62 @@
 #include "msg.h"
 
 #include <stdio.h>
-#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
 
 #include "macro.h"
 #include "log.h"
 #include "misc.h"
 
-_main() {
-//    log1("%lu,%lu", sizeof(msgReqInit), sizeof(msgLog));
+struct testOption {
+    int pubInterval;
+    int pubCount;
+};
+
+enum {
+    TEST_OK,
+    TEST_MISS_VALUE,
+    TEST_UNKNOWN_OPTION,
+};
+
+mainEx(argc, argv) {
+    struct testOption option = {.pubInterval=100, .pubCount=0xfffffff};
+    int rc, opt;
+
+    rc = TEST_OK;
+    while ((opt = getopt(argc, argv, ":i:n:h")) != -1) {
+        switch (opt) {
+            case 'i':
+                option.pubInterval = atoi(optarg);
+                break;
+            case 'n':
+                option.pubCount = atoi(optarg);
+                break;
+            case ':':
+                rc = TEST_MISS_VALUE;
+                goto usage;
+            case '?':
+                rc = TEST_UNKNOWN_OPTION;
+                goto usage;
+            case 'h':
+            usage:
+                log2("Usage: %s\n"
+                     "  -i $interval\n"
+                     "        publish interval(ms)\n"
+                     "  -n $count\n"
+                     "        total publish count", argv[0]);
+                return rc;
+            default:
+                __builtin_unreachable();
+        }
+    }
+
     log1("Log init:%d", logInit("logtest"));
 
     int i = 0;
-    for (;;) {
-        logPost(LOG_LEVEL_INFO, "posting #%d", ++i);
-        sleepMs(1000);
+    for (; i < option.pubCount; i++) {
+        logPost(LOG_LEVEL_INFO, "posting #%d", i);
+        sleepMs(option.pubInterval);
     }
     return 0;
 }
